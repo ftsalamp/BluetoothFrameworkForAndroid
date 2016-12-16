e60658e19b8e62b55ff4271b00710d138a1e4e70
ioanpier <ioanpier@gmail.com>
2016-12-16 20:00:47 +0200

    Initial commit

---
+++ app/src/main/java/grioanpier/auth/users/bluetoothframework/MainActivity.java
@@ -0,0 +1,13 @@
+package grioanpier.auth.users.bluetoothframework;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
